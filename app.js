946c30ffdea4e9fb6ad9f4d5171e5df4341f0523
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-08-28 18:53:53 +0200

    dotenv production

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-/**
- * Load environment variables from .env file, where API keys and passwords are configured.
- */
-dotenv.load({ path: '.env' });
+if (process.env.NODE_ENV !== 'production') {
+  /**
+   * Load environment variables from .env file, where API keys and passwords are configured.
+   */
+  dotenv.load({ path: '.env' });
+}
 
 /**
  * Controllers (route handlers).
