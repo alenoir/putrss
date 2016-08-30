b975535a9cb87d0560794d76aeb3af027e9b1751
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-08-30 10:19:15 +0200

    fix .env file for test

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const errorHandler = require('errorhandler');
 const lusca = require('lusca');
+const fileExists = require('file-exists');
 const dotenv = require('dotenv');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
@@ -19,7 +20,7 @@ const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-if (process.env.NODE_ENV !== 'production') {
+if (fileExists('.env')) {
   /**
    * Load environment variables from .env file, where API keys and passwords are configured.
    */
