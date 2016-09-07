0dcb74f19dbbc6e0abc09def789f3ee232dbffca
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-09-07 10:24:56 +0200

    remove random url

--- test/app.js
+++ test/app.js
@@ -8,11 +8,3 @@ describe('GET /', () => {
       .expect(200, done);
   });
 });
-
-describe('GET /random-url', () => {
-  it('should return 404', (done) => {
-    request(app)
-      .get('/reset')
-      .expect(404, done);
-  });
-});
